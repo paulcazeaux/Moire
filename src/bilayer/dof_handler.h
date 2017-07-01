d5e41fa69a0dadda939602f72ebe2495f66a8bf3
Paul Cazeaux <pcazeaux@umn.edu>
2017-06-30 21:36:24 -0500

    Adds Trilinos template definitions to solve missing instantiations issue.

--- src/bilayer/dof_handler.h
+++ src/bilayer/dof_handler.h
@@ -21,7 +21,9 @@
 #include <Teuchos_GlobalMPISession.hpp>
 #include <Tpetra_DefaultPlatform.hpp>
 #include <Tpetra_Map_decl.hpp>
+#include <Tpetra_Map_def.hpp>
 #include <Tpetra_CrsGraph_decl.hpp>
+#include <Tpetra_CrsGraph_def.hpp>
 
 #include "deal.II/base/exceptions.h"
 #include "deal.II/base/point.h"
