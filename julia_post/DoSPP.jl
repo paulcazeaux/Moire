1c68b3e79dbead4949bccae8b145f20d6628536e
Paul Cazeaux <pcazeaux@umn.edu>
2017-07-07 23:37:55 -0500

    Update Julia post processing files.

--- julia_post/DoSPP.jl
+++ julia_post/DoSPP.jl
@@ -3,17 +3,16 @@ using TB1
 using PyPlot
 pygui(true)
 
-n, nratios, Cheb_Moments = Read_DoS("bi_graphene003.jld")
+n, nratios, Cheb_Moments = Read_DoS("bi_graphene002.jld")
 
-n = 1500;
 Cheb_Moments = Cheb_Moments[1:n, :];
 
 Energies, DofS = DoS(11.5, 2, Cheb_Moments)
 
 clf()
 plot(Energies, DofS)
-ax = axes()
-ax[:set_xlim]([-1.2, 1.2])
-ax[:set_ylim]([0, 2])
+# ax = axes()
+# ax[:set_xlim]([-1.2, 1.2])
+# ax[:set_ylim]([0, 2])
 
-savefig("DoS.bi_graphene003.pdf")
+savefig("DoS.bi_graphene002.pdf")
--- julia_post/LDoSPP.jl
+++ julia_post/LDoSPP.jl
@@ -3,10 +3,20 @@ using TB1
 using PyPlot
 pygui(true)
 
-n, n_nodes, Cheb_Moments = Read_LDoS("/Users/cazeaux/Dropbox/Workplace/Projets_actuels/Minnesota/Calgebras_1D/Cpp/Output/test/bi_graphene002.jld")
+n, n_nodes, Cheb_Moments = Read_LDoS("bi_graphene_0.jld")
+
 
 clf()
-Energies, DofS = DoS(11.5, 2, Cheb_Moments[29, :])
-plot(Energies, DofS)
-Energies, DofS = DoS(11.5, 2, Cheb_Moments[30, :])
-plot(Energies, DofS)
+for m=0:5
+for n=0:5
+    # Energies, DofS = DoS(11.5, 2, Cheb_Moments[(m + 6*n)*2+1, :])
+    # plot(Energies, DofS)
+    # Energies, DofS = DoS(11.5, 2, Cheb_Moments[(m + 6*n)*2+2, :])
+    # plot(Energies, DofS)
+    Energies, DofS = DoS(11.5, 2, squeeze(sum(Cheb_Moments[(m + 6*n)*2+(1:2),:], 1), 1)/2)
+    plot(Energies, DofS)
+ end
+end
+ax = axes()
+ax[:set_xlim]([-2, 2]);
+ax[:set_ylim]([0, 2]);
